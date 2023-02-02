d206f4a36d67a5a874cdb03c6909638578c8a457
Felipe Duarte <vitor.avila@tesigroup.tech>
2023-02-02 16:12:50 -0300

    Adicionado formulario de local da assinatura

---
+++ TesiToSignBirdID/Views/SignatureLocation.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SignBirdID.Views
+{
+    partial class SignatureLocation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbx = new System.Windows.Forms.PictureBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.cbxShowLocation = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(511, 72);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbxShowLocation);
+            this.groupBox2.Controls.Add(this.btnOk);
+            this.groupBox2.Location = new System.Drawing.Point(12, 612);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(511, 98);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // pbx
+            // 
+            this.pbx.Location = new System.Drawing.Point(34, 90);
+            this.pbx.Name = "pbx";
+            this.pbx.Size = new System.Drawing.Size(470, 520);
+            this.pbx.TabIndex = 2;
+            this.pbx.TabStop = false;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnOk.Location = new System.Drawing.Point(362, 48);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(138, 39);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Aceitar";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // cbxShowLocation
+            // 
+            this.cbxShowLocation.AutoSize = true;
+            this.cbxShowLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxShowLocation.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.cbxShowLocation.Location = new System.Drawing.Point(11, 15);
+            this.cbxShowLocation.Name = "cbxShowLocation";
+            this.cbxShowLocation.Size = new System.Drawing.Size(344, 21);
+            this.cbxShowLocation.TabIndex = 1;
+            this.cbxShowLocation.Text = "Sempre perguntar o local para a assinatura";
+            this.cbxShowLocation.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(11, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(494, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecione o local para a assinatura no documento";
+            // 
+            // SignatureLocation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(535, 722);
+            this.Controls.Add(this.pbx);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SignatureLocation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SignatureLocation";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbxShowLocation;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.PictureBox pbx;
+    }
+}
